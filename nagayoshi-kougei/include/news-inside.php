926c656415ff4f117f524a35e852538d01e76a7e
神園巧真 <>
2023-10-29 22:53:35 +0900

    お知らせセクション部分の制作

--- nagayoshi-kougei/include/news-inside.php
+++ nagayoshi-kougei/include/news-inside.php
@@ -1,15 +1,21 @@
+
 <li id="post-<?php the_ID(); ?>"<?php post_class('news__box item') ?> >
 <a href="<?php the_permalink(); ?>">
-                                 
+<div class="news__thumbnail">
+<?php the_post_thumbnail(); ?>
+</div>
+<div class="news__sentence">                             
                        <ul class="cat fontSerif">
                        <?php categories_label(); ?>
                        </ul>
                    
                    <time><?php the_time( get_option( 'date_format' ) ); ?></time>  
-                   <p class="news__des"><?php the_title(); ?></p>
-                   </a>                      
-                  
+                   <h3 class="news__tit"><?php the_title(); ?></h3>
+                   <p class="news__desc"><?php the_excerpt(); ?></p>                                       
+                   </div>
+                   </a>   
                 </li>
+                
              
 
                 
--- nagayoshi-kougei/include/news.php
+++ nagayoshi-kougei/include/news.php
@@ -1,15 +1,24 @@
 <section class="news" >
-            <div class="section-titles">
-                  <h2 class="main-title">
-                    NEWS
+            <div class="section-titles-left">
+              <div class="news__top-warp">
+                  <h2 class="main-title-left">
+                    NEWS <span>お知らせ情報</span>
                   </h2>
+                  <div class="news__btn appear up">
+                <?php
+                      $news = get_term_by('slug','news','category');
+                      $news_link = get_term_link($news,'category')
+                      ?>
+                    <a href="<?= esc_url($news_link); ?>"  class="btn slide-bg item">過去の記事を見る</a>
+                  </div>
+                  </div>
                 </div>
                 <div  class="news__inner">
                 <?php
                      $args=[
                        'post_type'=> 'post',
                        'category_name' => 'news',
-                       'posts_per_page' => 5,        
+                       'posts_per_page' => 2,        
                      ];
                      $the_query = new WP_Query($args);
                     ?>
@@ -19,13 +28,7 @@
             <?php get_template_part('include/news-inside'); ?>
             <?php endwhile; ?>
                   </ul>
-                  <div class="news__btn appear up">
-                <?php
-                      $news = get_term_by('slug','news','category');
-                      $news_link = get_term_link($news,'category')
-                      ?>
-                    <a href="<?= esc_url($news_link); ?>"  class="btn slide-bg item">more</a>
-                  </div>
+                 
                   <?php else: ?>
                     <div class="notice__notinfo">
                       <p>新しい情報はありません</p>
