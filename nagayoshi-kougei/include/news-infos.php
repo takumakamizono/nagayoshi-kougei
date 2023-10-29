bde5be52173b19f301bd4941eb3c337a46f4d77c
神園巧真 <>
2023-10-29 22:50:34 +0900

    お知らせコンテンツセクションの制作
    facebookとNEWSのを2カラムにレイアウト

---
+++ nagayoshi-kougei/include/news-infos.php
@@ -0,0 +1,6 @@
+<section class="news-infos">
+    <div class="news-infos__inner">
+        <?php get_template_part('include/sns-contents'); ?>
+    <?php get_template_part('include/news'); ?>
+    </div>
+</section>
---
+++ nagayoshi-kougei/include/sns-contents.php
@@ -0,0 +1,10 @@
+<div class="sns-content">
+<div class="section-titles-left">
+                  <h2 class="main-title-left">
+                    fasebook <span>フェイスブック更新中</span>
+                  </h2>
+                </div>
+    <div class="sns-content">
+
+    </div>
+</div>
