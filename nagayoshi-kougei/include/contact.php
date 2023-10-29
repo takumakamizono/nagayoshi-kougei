1b0d27e9364dbba2e478db503d3a1427dc252d0f
神園巧真 <>
2023-10-29 22:54:38 +0900

    コンタクトセクションの制作をしました

---
+++ nagayoshi-kougei/include/contact.php
@@ -0,0 +1,16 @@
+<section class="contact">
+<div class="section-titles">
+                  <h2 class="main-title">
+                    CONTACT
+                    <span>企画・製作・施工のご相談！</span>
+                  </h2>
+                </div>
+    <div class="contact__inner">
+      <p>株式会社ナガヨシ巧芸では、文章が入ります文章が入ります文章が入ります文章が入ります文章が入ります文章が入ります...。
+      文章が入ります文章が入ります文章が入ります....お悩みの際はお気軽にご相談くださいませ。</p>
+      <div class="contact__btn up">
+        <a class="btn slide-bg item" href="">ご相談はこちらから</a>
+      </div>
+
+    </div>
+</section>
