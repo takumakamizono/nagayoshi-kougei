8aeb69c12b8ecdd4917b751ee48276b5a737df46
神園巧真 <>
2023-10-29 22:40:16 +0900

    productセクションの制作
    各制作物カテゴリページにリンクするブロックを設定

---
+++ nagayoshi-kougei/include/product.php
@@ -0,0 +1,75 @@
+<section class="product">
+    <div class="product__inner">
+    <div class="section-titles">
+            <h3 class="main-title">product<span>これまでの制作物の特集！</span></h3>
+        </div>
+        <ul class="menu-list">
+            <li class="menu-list__item">
+                <div class="hovereffect">
+                    <img class="bg-blur-img" src="<?= get_template_directory_uri(); ?>/images/menu_bg05.png" alt="">
+                    <div class="overlay">
+                    <h2>新築物件</h2>
+                    <a class="info" href="/tokushu/new.html">New construction</a>
+                    </div>
+                </div>
+            </li>
+            <li class="menu-list__item">
+                <div class="hovereffect">
+                    <img class="bg-blur-img" src="<?= get_template_directory_uri(); ?>/images/menu_bg05.png" alt="">
+                    <div class="overlay">
+                    <h2>新築物件</h2>
+                    <a class="info" href="/tokushu/new.html">New construction</a>
+                    </div>
+                </div>
+            </li>
+            <li class="menu-list__item">
+                <div class="hovereffect">
+                    <img class="bg-blur-img" src="<?= get_template_directory_uri(); ?>/images/menu_bg05.png" alt="">
+                    <div class="overlay">
+                    <h2>新築物件</h2>
+                    <a class="info" href="/tokushu/new.html">New construction</a>
+                    </div>
+                </div>
+            </li>
+            <li class="menu-list__item">
+                <div class="hovereffect">
+                    <img class="bg-blur-img" src="<?= get_template_directory_uri(); ?>/images/menu_bg05.png" alt="">
+                    <div class="overlay">
+                    <h2>新築物件</h2>
+                    <a class="info" href="/tokushu/new.html">New construction</a>
+                    </div>
+                </div>
+            </li>
+            <li class="menu-list__item">
+                <div class="hovereffect">
+                    <img class="bg-blur-img" src="<?= get_template_directory_uri(); ?>/images/menu_bg05.png" alt="">
+                    <div class="overlay">
+                    <h2>新築物件</h2>
+                    <a class="info" href="/tokushu/new.html">New construction</a>
+                    </div>
+                </div>
+            </li>
+            <li class="menu-list__item">
+                <div class="hovereffect">
+                    <img class="bg-blur-img" src="<?= get_template_directory_uri(); ?>/images/menu_bg05.png" alt="">
+                    <div class="overlay">
+                    <h2>新築物件</h2>
+                    <a class="info" href="/tokushu/new.html">New construction</a>
+                    </div>
+                </div>
+            </li>
+            <li class="menu-list__item">
+                <div class="hovereffect">
+                    <img class="bg-blur-img" src="<?= get_template_directory_uri(); ?>/images/menu_bg05.png" alt="">
+                    <div class="overlay">
+                    <h2>新築物件</h2>
+                    <a class="info" href="/tokushu/new.html">New construction</a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+
+       
+    </div>
+</section>
